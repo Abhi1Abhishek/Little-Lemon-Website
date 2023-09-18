6130a6168e9179fa67cd4fe53ae7c0c2f69e4dfe
Abhishek <145200237+Abhi1Abhishek@users.noreply.github.com>
2023-09-18 07:52:24 +0000

    Semantic Structure

--- littlelemon/src/App.js
+++ littlelemon/src/App.js
@@ -3,7 +3,17 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Home Page</h1>
+      <headers/>
+      <nav>
+        <ul>
+          <li><a href="/home">Homepage</a></li>
+          <li><a href="/about">Aboutpage</a></li>
+          <li><a href="/blog">Blogpage</a></li>
+        </ul>
+      </nav>
+      <main/>
+      <footer/>
+      <a href="/book" role="button">Book a Table</a>
     </div>
   );
 }
