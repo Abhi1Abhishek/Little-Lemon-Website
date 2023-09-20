084e84190c3b5a8a84ab1c5e947be8bf85d085a3
Abhishek <145200237+Abhi1Abhishek@users.noreply.github.com>
2023-09-20 17:37:57 +0000

    Integrating Navbar Section

--- littlelemon/src/App.js
+++ littlelemon/src/App.js
@@ -1,24 +1,17 @@
+import React from 'react';
 import './App.css';
-import Header from "./Header";
-import Footer from "./Footer";
-import Main from "./Main";
-import Nav from "./Nav";
+// import Header from "./Header";
+// import Footer from "./Footer";
+// import Main from "./Main";
+import Nav from "./components/Nav";
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <nav>
-        <ul>
-          <li><a href="/home">Homepage</a></li>
-          <li><a href="/about">Aboutpage</a></li>
-          <li><a href="/blog">Blogpage</a></li>
-        </ul>
-      </nav>
-      <Nav/>
-      <Main/>
-      <Footer />
-      <a href="/book" role="button">Book a Table</a>
-    </div>
+    // <div className="App">
+    <>
+    <Nav/>
+    </>
+
+    // </div>
   );
 }
 
--- littlelemon/src/Nav.js
+++
@@ -1,8 +0,0 @@
-
-export default function Nav() {
-        return (
-            <div className="main-grid">
-                <h1>This is the Nav</h1>
-            </div>
-        )
-    }
