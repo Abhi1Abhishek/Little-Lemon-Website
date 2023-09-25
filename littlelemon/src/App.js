15f9793bdd588e285256f8352321a8d1e2ea1777
Abhishek <145200237+Abhi1Abhishek@users.noreply.github.com>
2023-09-25 05:56:39 +0000

    Add Header Components

--- littlelemon/src/App.js
+++ littlelemon/src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import './App.css';
-// import Header from "./Header";
 // import Footer from "./Footer";
 // import Main from "./Main";
 import Nav from "./components/Nav";
+import Header from "./components/Header";
 function App() {
   return (
     // <div className="App">
     <>
     <Nav/>
+    <Header />
     </>
 
     // </div>
